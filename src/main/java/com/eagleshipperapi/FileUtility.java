5986d877d9274089031ab3b1eeb9e8a382e7b354
Tanveer_patel <sultanpatel828@gmail.com>
2020-11-07 13:40:46 +0530

    Updated backen api

--- src/main/java/com/eagleshipperapi/FileUtility.java
+++ src/main/java/com/eagleshipperapi/FileUtility.java
@@ -4,36 +4,40 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.HashMap;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
 public class FileUtility {
 
 	public String getImageUrl(MultipartFile file) throws IOException {
 		InputStream serviceAccount = this.getClass().getClassLoader().getResourceAsStream("./serviceAccountKey.json");
-		StorageOptions.newBuilder().setProjectId("eagleshippers-f384c").
-  	    		setCredentials(GoogleCredentials.fromStream(serviceAccount)).build().getService();
+		Storage storage = StorageOptions.newBuilder().setProjectId("eagleshippers-f384c")
+			.setCredentials(GoogleCredentials.fromStream(serviceAccount)).build().getService();
 		HashMap<String, String> hm = new HashMap<>();
    	    hm.put("firebaseStorageDownloadTokens", "3434434343434dfdf");
   
    	    BlobId blobId = BlobId.of("eagleshippers-f384c.appspot.com",file.getOriginalFilename());
 		     
-		    BlobInfo.newBuilder(blobId).
+		BlobInfo blobInfo = BlobInfo.newBuilder(blobId).
 		    		setContentType("image/jpeg")
 		    		.setMetadata(hm)
 		    		.build();
 		    
-		    File convertedFile = new File(file.getOriginalFilename());
+		File convertedFile = new File(file.getOriginalFilename());
         FileOutputStream fos = new FileOutputStream(convertedFile);
         fos.write(file.getBytes());
         fos.close();
-       
+        @SuppressWarnings("unused")
+		Blob blob = storage.create(blobInfo, Files.readAllBytes(convertedFile.toPath()));
 	    String imageUrl = "https://firebasestorage.googleapis.com/v0/b/eagleshippers-f384c.appspot.com/o/"+convertedFile+"?alt=media&token=3434434343434dfdf";
 	    return imageUrl;
 	}
--- src/main/java/com/eagleshipperapi/service/TransporterService.java
+++ src/main/java/com/eagleshipperapi/service/TransporterService.java
@@ -1,8 +1,74 @@
 package com.eagleshipperapi.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.eagleshipperapi.FileUtility;
+import com.eagleshipperapi.bean.Transporter;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Service
 public class TransporterService {
 
+	private static final String TAG = "Transporter";
+	private Firestore fireStore = FirestoreClient.getFirestore();
+	
+	//get all transporter
+	public ArrayList<Transporter> getTransporter() throws InterruptedException, ExecutionException{
+		ArrayList<Transporter>transporterList = new ArrayList<Transporter>();
+		List<QueryDocumentSnapshot> list = fireStore.collection(TAG).get().get().getDocuments();	
+		for(QueryDocumentSnapshot queryDocument : list ) {
+			transporterList.add(queryDocument.toObject(Transporter.class));
+		}
+		return transporterList;		
+	}
+	
+	
+	//get Single Transporter
+	public Transporter getTransporter(String id) throws InterruptedException, ExecutionException {
+		Transporter t = fireStore.collection(TAG).document(id).get().get().toObject(Transporter.class);
+		return t;
+	}
+	
+	// create new transporter
+	public Transporter createTransporter(Transporter transporter,MultipartFile file) throws IOException {
+			
+		String imageUrl = new FileUtility().getImageUrl(file);
+		transporter.setImageUrl(imageUrl);
+		String id = fireStore.collection(TAG).document().getId();
+		transporter.setTransporterId(id);
+		fireStore.collection(TAG).document(id).set(transporter);
+		return transporter;
+	}
+	
+	//delete Transporter by id
+	public Transporter deleteTransporter(String id) throws InterruptedException, ExecutionException {
+		Transporter t = fireStore.collection(TAG).document(id).get().get().toObject(Transporter.class);
+		fireStore.collection(TAG).document(id).delete();
+		return t;
+	}
+	
+	//update transporter details
+	public Transporter updateTransporter(Transporter transporter) throws IOException, InterruptedException, ExecutionException {
+		fireStore.collection(TAG).document(transporter.getTransporterId()).set(transporter);
+		Transporter t = fireStore.collection(TAG).document(transporter.getTransporterId()).get().get().toObject(Transporter.class);
+		return t;
+	}
+	
+	//update transporter image
+		public Transporter updateTransporter(String transporterId,MultipartFile file) throws IOException, InterruptedException, ExecutionException {
+			Transporter t = fireStore.collection(TAG).document(transporterId).get().get().toObject(Transporter.class);
+			String imageUrl =  new FileUtility().getImageUrl(file);
+			t.setImageUrl(imageUrl);
+			fireStore.collection(TAG).document(transporterId).set(t);
+			return t;
+		}
+	
 }
